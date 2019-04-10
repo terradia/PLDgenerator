a75f523a334a06fc060ef12f085db2e2b4653e04
heim_v <victor.heim@epitech.eu>
2019-04-11 01:19:11 +0200

    adding cellgroup class

--- test.py
+++ test.py
@@ -1,5 +1,53 @@
 import asana
 import sys
+import random
+from enum import Enum
+from datetime import datetime
+
+
+BeginFile = '<mxGraphModel dx="1394" dy="824" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1.5" pageWidth="1169" pageHeight="827" background="#ffffff" math="0" shadow="0">\n    <root>\n        <mxCell id="0"/>\n        <mxCell id="1" parent="0"/>\n'
+EndFile = '    </root>\n</mxGraphModel>\n'
+
+CellId = 2
+
+class CellType(Enum):
+    ROOT = 0
+    DELIVERABLE = 1
+    CARD = 2
+
+
+class GroupCell:
+    def __init__(self, f, level = CellType.ROOT):
+        global CellId
+        self.padding = 20
+        self.rand = lambda: random.randint(0, 255)
+        self.ID = CellId
+        CellId += 1
+        if level == CellType.ROOT:
+            self.color = "#AE4132"
+            self.x = -105
+            self.y = 0
+            self.width = 200
+            self.height = 80
+        elif level == CellType.DELIVERABLE:
+            self.color = "#10739E"
+            self.x = -105
+            self.y = 50
+            self.width = 200
+            self.height = 80
+        elif level == CellType.CARD:
+            self.color = '#{:02x}{:02x}{:02x}'.format(self.rand(), self.rand(), self.rand())
+            self.x = 0
+            self.y = 170
+            self.width = 200
+            self.height = 80
+        self.render()
+
+    def render(self):
+        f.write('<mxCell id="{}" value="" style="fillColor={};strokeColor=none;opacity=30;" parent="1" vertex="1">\n'
+                '   <mxGeometry x="{}" y="{}" width="{}" height="{}" as="geometry"/>\n'
+                '</mxCell>\n'.format(self.ID, self.color, self.x, self.y, self.width, self.height))
+
 
 
 def dump(obj, nested_level=0, output=sys.stdout):
@@ -46,6 +94,9 @@ for deliverable in PLD:
         cards[tabs["name"]] = subs
     diag[deliverable["name"]] = cards
 dump(diag)
-
-
+filename = "Deliverable " + str(datetime.now().month) + "-" + str(datetime.now().year) + ".xml"
+f = open(filename, "w")
+f.write(BeginFile)
+GroupCell(f, CellType.ROOT)
+f.write(EndFile)
 
